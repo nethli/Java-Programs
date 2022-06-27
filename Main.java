6db7f4f3f2fddfd6358cd15f688b1f3d13e104c5
isharanethli <87275026+nethli@users.noreply.github.com>
2022-06-27 20:04:25 +0530

    Equality Printer

--- Main.java
+++ Main.java
@@ -1,19 +1,5 @@
-package acedamy.learnprogramming;
-
 public class Main {
-
     public static void main(String[] args) {
-        byte byteVar   = 25;
-        short shortVar = 10;
-        int intVar     = 5;
-
-        /*Here we don't need casting with the long because the long will happily accept the integer. Because the maximum
-         integer value will always fit within a long value. */
-        long longVar   =  50000L + 10L* (byteVar + shortVar+ intVar);
-
-        System.out.println(longVar);
-
-
-
+        EqualityPrinter.printEqual(1,3,2);
     }
 }
