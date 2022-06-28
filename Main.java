04e1098b6753f41f22088a59357a6f0e0d522890
isharanethli <87275026+nethli@users.noreply.github.com>
2022-06-28 12:13:35 +0530

    Carpet Cost Calculator

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+package acedamy.learnprogramming;
+
 public class Main {
+
     public static void main(String[] args) {
-        EqualityPrinter.printEqual(1,3,2);
+        Carpet carpet = new Carpet(3.5);
+        Floor floor = new Floor(2.75, 4.0);
+        Calculator calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+
+        carpet = new Carpet(1.5);
+        floor = new Floor(5.4, 4.5);
+        calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
     }
 }
